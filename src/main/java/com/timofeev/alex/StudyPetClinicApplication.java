70879ab6e9b32969e19a1ffacd9a262bffaaaf24
Timofeev Alex <alexandr.tymofieiev>
2020-05-11 01:24:38 +0300

    Initial Project

---
+++ src/main/java/com/timofeev/alex/StudyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.timofeev.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPetClinicApplication.class, args);
+	}
+
+}
